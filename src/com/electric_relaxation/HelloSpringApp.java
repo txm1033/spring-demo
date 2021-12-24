3a60cb1a6de80dcf7d545514804c294aefe4bc6b
txm1033 <tacia_mitchell@outlook.com>
2021-12-23 21:34:37 -0500

    created spring app and beans

--- src/com/electric_relaxation/HelloSpringApp.java
+++ src/com/electric_relaxation/HelloSpringApp.java
@@ -1,7 +1,23 @@
 package com.electric_relaxation;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpringApp {
     public static void main(String[] args) {
+
+        // load the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        CoachInterface coach = context.getBean("myCoach",CoachInterface.class);
+        CoachInterface trackCoach = context.getBean("trackCoach",CoachInterface.class);
+
+        //call methods on the bean
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(trackCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
 
     }
 }
--- src/com/electric_relaxation/MyApp.java
+++ src/com/electric_relaxation/MyApp.java
@@ -3,11 +3,6 @@ package com.electric_relaxation;
 public class MyApp {
     public static void main(String[] args) {
 
-        CoachInterface theCoach = new BaseballCoach();
-        CoachInterface trackCoach = new TrackCoach();
-        System.out.println(theCoach.getDailyWorkout());
-        System.out.println(trackCoach.getDailyWorkout());
-
 
     }
 }
